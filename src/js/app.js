dbad047998b7002a0c3ab456a94ebfe975bc7407
emiaesthetic <emi.aesthetic@gmail.com>
2024-11-25 08:21:01 +0500

    Create class TomatoTimer

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,14 @@
 import '../index.html';
 import '../scss/app.scss';
+
+import { TomatoTimer } from './modules/timer.js';
+
+const timer = new TomatoTimer('Course', 2);
+console.log(timer.getID());
+console.log(timer.getName());
+console.log(timer.getCounter());
+
+timer.setName('English').setCounter(1);
+console.log(timer.getID());
+console.log(timer.getName());
+console.log(timer.getCounter());
