08002fa995df41719f593be03de40e6f1feab0ea
emiaesthetic <emi.aesthetic@gmail.com>
2025-01-01 16:09:35 +0500

    Fix counter interaction

--- src/js/views/counterView.js
+++ src/js/views/counterView.js
@@ -1,43 +1,91 @@
 export class CounterView {
   constructor() {
     this.counter = document.querySelector('.counter');
-    this.labels = this.counter.querySelectorAll('.counter__label');
-    this.radios = this.counter.querySelectorAll('.counter__radio');
-    this.openPopupBtn = this.counter.querySelector('.counter__open-popup');
+    this.alarms = this.counter.querySelectorAll('.counter__alarm');
+    this.togglePopupBtn = this.counter.querySelector('.counter__toggle-popup');
     this.popup = this.counter.querySelector('.counter__popup');
     this.popupDisplay = this.counter.querySelector('.counter__display');
     this.popupInput = this.counter.querySelector('.counter__input');
     this.increaseBtn = this.counter.querySelector('.counter__increase');
     this.decreaseBtn = this.counter.querySelector('.counter__decrease');
 
-    this.openPopupBtn.addEventListener('click', this.togglePopup.bind(this));
+    this.togglePopupBtn.addEventListener('click', this.togglePopup.bind(this));
+    this.handleEscKey = this.handleEscKey.bind(this);
+    this.handleClickOutside = this.handleClickOutside.bind(this);
   }
 
   togglePopup() {
     this.popup.classList.toggle('counter__popup--is-open');
+    this.updatePopupState();
   }
 
-  bindRadioChange(handler) {
-    this.radios.forEach((radio, index) => {
-      this.handleRadioChange(radio, index, handler);
+  updatePopupState() {
+    const isOpen = this.popup.classList.contains('counter__popup--is-open');
+    this.updateAriaExpanded(isOpen);
+    this.updateToggleButtonLabel(isOpen);
+    this.toggleEventListeners(isOpen);
+  }
+
+  updateAriaExpanded(isExpanded) {
+    this.popup.ariaExpanded = isExpanded;
+  }
+
+  updateToggleButtonLabel(isOpen) {
+    this.togglePopupBtn.ariaLabel = isOpen
+      ? 'Close counter popup'
+      : 'Open counter popup';
+  }
+
+  toggleEventListeners(shouldAdd) {
+    if (shouldAdd) {
+      this.attachPopupEventListeners();
+    } else {
+      this.detachPopupEventListeners();
+    }
+  }
+
+  attachPopupEventListeners() {
+    document.addEventListener('keydown', this.handleEscKey);
+    document.addEventListener('click', this.handleClickOutside);
+  }
+
+  detachPopupEventListeners() {
+    document.removeEventListener('keydown', this.handleEscKey);
+    document.removeEventListener('click', this.handleClickOutside);
+  }
+
+  handleEscKey(event) {
+    if (event.key === 'Escape') {
+      this.togglePopup();
+    }
+  }
+
+  handleClickOutside(event) {
+    if (
+      !this.popup.contains(event.target) &&
+      !this.togglePopupBtn.contains(event.target)
+    ) {
+      this.togglePopup();
+    }
+  }
+
+  bindAlarmClick(handler) {
+    this.alarms.forEach((alarm, index) => {
+      alarm.addEventListener('click', () => {
+        this.highlightAlarmsByIndex(index);
+        handler(index + 1);
+      });
     });
   }
 
-  highlightLabelsByIndex(index) {
-    this.labels.forEach((label, currentIndex) => {
+  highlightAlarmsByIndex(index) {
+    this.alarms.forEach((label, currentIndex) => {
       label.classList.toggle('selected', currentIndex <= index);
     });
   }
 
-  handleRadioChange(radio, index, handler) {
-    radio.addEventListener('change', () => {
-      this.highlightLabelsByIndex(index);
-      handler(parseInt(radio.value, 10));
-    });
-  }
-
-  highlightLabelsByValue(value) {
-    this.highlightLabelsByIndex(value - 1);
+  highlightAlarmsByValue(value) {
+    this.highlightAlarmsByIndex(value - 1);
   }
 
   bindDecrease(handler) {
